b22af1ccb2ce10e6a09b3c422458e3428210a080
Quentin Martinez <quentin.martinez@outlook.com>
2017-05-30 11:31:15 +0200

    initial fork commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var request=require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,6 +12,12 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+app.post('/', function(req, res){
+  request.get('http://raspberrydomotic.ddns.net:8080/json.htm?username=YWRtaW4=&password=em52OGEyYjU=&type=command&param=switchlight&idx=1&switchcmd=Off',options,function(err,res,body){
+  if(!err && res.statusCode === 200){
+   
+   }});
 });
 
 app.listen(app.get('port'), function() {
